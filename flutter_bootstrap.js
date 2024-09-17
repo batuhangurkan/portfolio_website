acbf4688ad685c43b177ce927ed4089547fac903
Batuhan GÜRKAN <batuhan108668@gmail.com>
2024-09-18 00:14:06 +0300

    İlk commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2912592961"
+    serviceWorkerVersion: "1970087122"
   }
 });
